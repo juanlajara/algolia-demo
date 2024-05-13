a023fed845d7fce793abf9626cab06b43dc7d28f
juanlajara <juanlajara001@gmail.com>
2024-05-13 17:10:46 -0500

    added left panel and brand list

--- ais-ecommerce-demo-app/src/app.js
+++ ais-ecommerce-demo-app/src/app.js
@@ -42,6 +42,22 @@ search.addWidgets([
   }),
 ]);
 
+search.addWidgets([
+  instantsearch.widgets.clearRefinements({
+    container: '#clear-refinements',
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#brand-list',
+    attribute: 'brand',
+  }),
+
+  instantsearch.widgets.configure({
+    hitsPerPage: 8
+  }),
+]);
+
+
 search.start();
 
 const recentSearchesPlugin = createLocalStorageRecentSearchesPlugin({
